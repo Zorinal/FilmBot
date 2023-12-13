fa1858f16861f552d1cd56132f5f26cb0b08745d
DmitriyChepasov <95770579+Zorinal@users.noreply.github.com>
2023-12-13 10:52:05 +0300

    Add files via upload

--- MiniBert/LSTM.py
+++ MiniBert/LSTM.py
@@ -5,6 +5,7 @@ import numpy as np
 import keras
 device = torch.device('cuda')
 MAX_SENTENCE_LEN = 512
+
 class MyLSTM(torch.nn.Module):
     def __init__(self,dimension, hidden_size, num_layers,out_size,bidirectional):
         super(MyLSTM,self).__init__()
@@ -25,8 +26,6 @@ class MyLSTM(torch.nn.Module):
         return out
 
 
-
-
 def semantic_sense(first_vector,all_vector):
     for i in range(len(all_vector)):
         try:
@@ -44,49 +43,57 @@ def soft_max(input_vector: np.array):
             input_vector[i] = input_vector[i] / sum_vector
     return input_vector
 
+
 def tokenize(input_sentences, dict_vocab):
     tokenize_sentences = np.zeros([MAX_SENTENCE_LEN])
     for i in range(len(input_sentences)):
         temp_sent = input_sentences[i].split()
         temp_tokenize_sentences = np.zeros([MAX_SENTENCE_LEN])
         for word in range(len(temp_sent)):
-            temp_val = dict_vocab[temp_sent[word]]
+            temp_val = dict_vocab[temp_sent[word].lower()]
             np.put(temp_tokenize_sentences, (0, word), temp_val)
         tokenize_sentences = np.vstack((tokenize_sentences, temp_tokenize_sentences))
     tokenize_sentences = np.delete(tokenize_sentences, 0, axis=0)
     return tokenize_sentences
 
 
+def create_vocab_and_sentences_and_answers(input_sentences):
+    vocab = []
+    sentences = []
+    answers = []
+    for index in range(len(input_sentences)):
+        try:
+            vocab.extend(list(map(lambda x: x, films["description"][index].split())))
+            vocab.extend(list(map(lambda x: x, films["title" or "movie_name"][index].split())))
+            sentences.append(films["description"][index])
+            answers.append(films['title'][index])
+        except:
+            continue
+
+    vocab = list(set(vocab))
+    dict_vocab = {}
+
+    for i in range(len(vocab)):
+        dict_vocab.update({vocab[i].lower(): i + 1})
+
+    return dict_vocab, sentences, answers
 
 
 imdb = pd.read_csv("../datasets/imdb.csv")
 films = pd.read_csv("../datasets/filmtv_movies.csv")
 # tokenize sentences by words
-vocab = []
-sentences = []
-answers = []
-for i in range(1500):
-    try:
-        vocab.extend(list(map(lambda x: x, films["description"][i].split())))
-        vocab.extend(list(map(lambda x: x, films["title"][i].split())))
-        sentences.append(films["description"][i])
-        answers.append(films['title'][i])
-    except:
-         continue
-vocab = list(set(vocab))
+dict_vocab = create_vocab_and_sentences_and_answers(films[:2000])[0]
+sentences = create_vocab_and_sentences_and_answers(films[:2000])[1]
+answers = create_vocab_and_sentences_and_answers(films[:2000])[2]
+print(dict_vocab)
 
-dict_vocab = {}
-
-for i in range(len(vocab)):
-    dict_vocab.update({vocab[i]: i + 1})
 tokenize_sentences = tokenize(sentences,dict_vocab)
 tokenize_answers = tokenize(answers,dict_vocab)
 
-for i in range(1500):
+for i in range(2000):
     tokenize_sentences[i] = semantic_sense(tokenize_sentences[i],tokenize_sentences)
     tokenize_sentences[i] = soft_max(tokenize_sentences[i])
     tokenize_answers[i] = soft_max(tokenize_answers[i])
-X_train, X_test, y_train, y_test = train_test_split(tokenize_sentences, tokenize_answers, test_size=0.2)
 criterion = torch.nn.MSELoss()
 dimension = 512
 hide_size = 5
@@ -95,10 +102,10 @@ out_size = 10
 bidirectional = True
 model = MyLSTM(dimension,hide_size,num_layers,out_size,bidirectional)
 num_epoch = 100
-loss = torch.nn.CrossEntropyLoss()
+loss = torch.nn.CrossEntropyLoss() #CEloss = -sum(target[i] * log(prediction[i]) or BCELoss = -1/N * sum(target[i] * log(prediction[i] + (1-target[i])log(1-prediction[i]))
 optimizer = torch.optim.Adam(model.parameters())
 for epoch in range(num_epoch):
-    for X,y in zip(tokenize_sentences, tokenize_answers):
+    for X,y in tokenize_sentences, tokenize_answers:
         optimizer.zero_grad()
         y_prediction = model(X.unsqueeze(-1))
         loss = loss(y_prediction.squeeze(),y)
