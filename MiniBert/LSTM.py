0232e9d54b30da0104cfa9004ccf40a7906a86d6
DmitriyChepasov <95770579+Zorinal@users.noreply.github.com>
2023-12-18 12:51:16 +0300

    Add files via upload

--- MiniBert/LSTM.py
+++ MiniBert/LSTM.py
@@ -19,7 +19,7 @@ class MyLSTM(torch.nn.Module):
         #h0 = torch.zeros(self.num_layer,sentence.size(0), self.hidden_size*self.num_layer,dtype=torch.float32)
         out,(h0,c0)= self.lstm(sentence)
         out = self.fc(h0)
-        out = torch.nn.functional.softmax(out,dim=1)
+        out = torch.nn.functional.sigmoid(out)
         return out
 
 
@@ -31,7 +31,10 @@ def tokenize(input_sentences, dict_vocab):
         temp_tokenize_sentences = np.zeros([MAX_SENTENCE_LEN])
         for word_ind in range(len(temp_sent)):
             key = [k for k,v in dict_vocab.items() if v == temp_sent[word_ind]]
-            np.put(temp_tokenize_sentences, (0, word_ind), key)
+            if key != []:
+                np.put(temp_tokenize_sentences, (0, key[0]-1), 1)
+            else:
+                np.put(temp_tokenize_sentences, (0, word_ind), -1)
         out_tokenize_sentences = np.vstack((out_tokenize_sentences, temp_tokenize_sentences))
     out_tokenize_sentences = np.delete(out_tokenize_sentences, 0, axis=0)
     return out_tokenize_sentences
@@ -60,9 +63,9 @@ def create_vocab_and_sentences_and_answers(input_sentences):
 
 films = pd.read_csv("../datasets/filmtv_movies.csv")
 # tokenize sentences by words
-dict_vocab = create_vocab_and_sentences_and_answers(films[:1100])[0]
-sentences = create_vocab_and_sentences_and_answers(films[:1100])[1]
-answers = create_vocab_and_sentences_and_answers(films[:1100])[2]
+dict_vocab = create_vocab_and_sentences_and_answers(films[:100])[0]
+sentences = create_vocab_and_sentences_and_answers(films[:100])[1]
+answers = create_vocab_and_sentences_and_answers(films[:100])[2]
 
 
 tokenize_sentences = tokenize(sentences,dict_vocab)
@@ -80,39 +83,37 @@ test_loader = DataLoader(test_dataset)
 dimension = len(dict_vocab)
 
 #model = MyLSTM(dimension,hide_size,num_layers,out_size)
-model = torch.jit.load('model_scripted1100softmax(x2layers,epochs+5).pt')
+model = torch.jit.load('model100softmax(30 num_layres, 1100-neurons) another tokenize.pt')
 model.eval()
-#model = model.to(device)
 num_epoch = 20
 #model.eval()
 loss = torch.nn.CrossEntropyLoss() #CEloss = -sum(target[i] * log(prediction[i]) or BCELoss = -1/N * sum(target[i] * log(prediction[i] + (1-target[i])log(1-prediction[i]))
 optimizer = torch.optim.Adam(model.parameters())
-print(model)
+
 #train process
 #for epoch in range(num_epoch):
- # for description,answer in train_loader:
-      #  optimizer.zero_grad()
-      #  y_prediction = model(description)
-       # _loss = loss(y_prediction, answer)
-        #_loss.backward()
-      #  optimizer.step()
+  #for description,answer in train_loader:
+       # optimizer.zero_grad()
+       # y_prediction = model(description)
+        #_loss = loss(y_prediction, answer)
+       # _loss.backward()
+        #optimizer.step()
 #model_scripted = torch.jit.script(model) # Export to TorchScript
-#model_scripted.save('model_scripted1100softmax(x2layers,epochs+5).pt') # Save
-#train = false
-#model.eval()
-#evulation results
+#model_scripted.save('model100sigmoid(30 num_layres, 1100-neurons) another tokenize.pt') # Save
+
+#evulation result
 #with torch.no_grad():
    # for description, answer in test_loader:
-    #    y_prediction = model(description)
-     #   print(y_prediction)
+      #  y_prediction = model(description)
+       # print(y_prediction)
+print(model)
 input_description = tokenize(str(input()), dict_vocab)
 input_description = torch.tensor(input_description,dtype=torch.float32)
 word = []
+possibility = []
 prediction = model(input_description)
-print(prediction)
 for i in range(len(prediction)):
-   word.append(torch.argmax(prediction[i]).item())
-#word = list(set(word))
+    word.append(torch.argmax(prediction[i]).item())
+possibility = torch.max(prediction,dim =1)
 for i in range(len(word)):
-    tmp = i+1
-    print(dict_vocab[tmp],end = ' ')
+    print(dict_vocab[word[i]+1],"->", possibility[0][i])
